10d1729b1b373ae075fe541dad04a37d3653aca6
Shourya Sinha <134903205+Shourya-Sinha@users.noreply.github.com>
2024-05-29 12:17:05 +0700

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,13 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-		proxy: {
-			"/api": {
-				target: "http://localhost:3000",
-			},
-		},
-	},
-});
+  plugins: [react()],
+})
+// export default defineConfig({
+// 	plugins: [react()],
+// 	server: {
+// 		port: 3000,
+// 		proxy: {
+// 			"/api": {
+// 				target: "http://localhost:3000",
+// 			},
+// 		},
+// 	},
+// });
